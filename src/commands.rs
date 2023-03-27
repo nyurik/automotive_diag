50e2300312d18aec5d214037d40b2c52301d5e9d
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2023-03-27 01:59:27 -0400

    refactored Commands to match the spec
    Removed `AccessTimingParameters = 0x83` -- not documented anywhere, must be a custom param

--- src/commands.rs
+++ src/commands.rs
@@ -3,42 +3,59 @@ use crate::utils::ByteWrapper;
 use bytenum::Bytenum;
 
 /// UDS Command Service IDs
-#[allow(missing_docs)]
 #[repr(u8)]
 #[derive(Bytenum, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum UdsCommand {
-    /// Diagnostic session control.
+    /// The client requests to control a diagnostic session with a server(s).
     DiagnosticSessionControl = 0x10,
-    /// ECU Reset.
-    ResetEcu = 0x11,
-    /// Security access.
+    /// The client forces the server(s) to perform a reset.
+    ECUReset = 0x11,
+    /// The client requests to unlock a secured server(s).
     SecurityAccess = 0x27,
-    /// Controls communication functionality of the ECU.
+    /// The client controls the setting of communication parameters in the server (e.g. communication baud-rate).
     CommunicationControl = 0x28,
-    /// Tester present command. Used internally by UDS Server
+    /// The client indicates to the server(s) that it is still present.
     TesterPresent = 0x3E,
-    /// Accesses ECU timing parameters.
-    AccessTimingParameters = 0x83,
+    /// The client uses this service to perform data transmission with an extended data link security.
     SecuredDataTransmission = 0x84,
-    ControlDtcSettings = 0x85,
+    /// The client controls the setting of DTCs in the server.
+    ControlDTCSetting = 0x85,
+    /// The client requests to set up and/or control an event mechanism in the server.
     ResponseOnEvent = 0x86,
+    /// The client requests control of the communication baud-rate.
     LinkControl = 0x87,
+    /// The client requests to read the current value of a record identified by a provided dataIdentifier.
     ReadDataByIdentifier = 0x22,
+    /// The client requests to read the current value of the provided memory range.
     ReadMemoryByAddress = 0x23,
+    /// The client requests to read the scaling information of a record identified by a provided dataIdentifier.
     ReadScalingDataByIdentifier = 0x24,
+    /// The client requests to schedule data in the server for periodic transmission.
     ReadDataByPeriodicIdentifier = 0x2A,
+    /// The client requests to dynamically define data Identifiers that may subsequently be read by the [`Self::ReadDataByIdentifier`] service.
     DynamicallyDefineDataIdentifier = 0x2C,
+    /// The client requests to write a record specified by a provided dataIdentifier.
     WriteDataByIdentifier = 0x2E,
+    /// The client requests to overwrite a provided memory range.
     WriteMemoryByAddress = 0x3D,
+    /// Allows the client to clear diagnostic information from the server (including DTCs, captured data, etc.).
     ClearDiagnosticInformation = 0x14,
-    /// Reading and querying diagnostic trouble codes stored on the ECU.
-    ReadDtcInformation = 0x19,
+    /// Allows the client to request diagnostic information from the server (including DTCs, captured data, etc.).
+    ReadDTCInformation = 0x19,
+    ///The client requests the control of an input/output specific to the server.
     InputOutputControlByIdentifier = 0x2F,
+    /// The client requests to start, stop a routine in the server(s) or requests the routine results.
     RoutineControl = 0x31,
+    ///The client requests the negotiation of a data transfer from the client to the server.
     RequestDownload = 0x34,
+    /// The client requests the negotiation of a data transfer from the server to the client.
     RequestUpload = 0x35,
+    /// The client transmits data to the server (download) or requests data from the server (upload).
     TransferData = 0x36,
+    /// The client requests the termination of a data transfer.
     RequestTransferExit = 0x37,
+    /// The client requests the negotiation of a file transfer between server and client.
+    RequestFileTransfer = 0x38,
 }
 
 byte_enum!(UdsCommand, UdsCommandByte);
